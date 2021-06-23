7f804a31b10a50a0ce3c660312fba3b0decfb5dc
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-06-23 07:25:52 -0500

    Instance Objects javascript

--- javascript/Class/Car.js
+++ javascript/Class/Car.js
@@ -1,6 +1,10 @@
-function Car() {
+function Car( license , drive ) {
 	this.id
-	this.license
-	this.driver
+	this.license = license
+	this.driver = drive
 	this.passanger
+}
+
+Car.prototype.printDataCar = function () {
+	console.log( this.driver.name,' ', this.license )
 }
---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+let car = new Car('AAA', new Account('Andres', '123' , 'andres@email.com'))
+car.passager = 4
+car.printDataCar()
