b47dc063941a9a1d87b40969121b0e08343b81c8
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-06-23 07:06:50 -0500

    Create Objects Python

--- python/main.py
+++ python/main.py
@@ -1,2 +1,12 @@
+from classes.car import Car
+
 if __name__ == "__main__":
-	print('Hola mundo')
+	car = Car()
+	car.license = 'AMS234'
+	car.driver = 'Juan H'
+	print(vars(car))
+
+	car2 = Car()
+	car2.license = 'AAAA'
+	car2.driver = 'Miguel'
+	print(vars(car2))
