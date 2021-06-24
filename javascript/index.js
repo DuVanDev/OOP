8d3defb10168d6896854762629f7411747545677
Duvan <duvanmorenocardona.dmc@gmail.com>
2021-06-24 07:11:03 -0500

    Import Class and use

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,7 @@
 let car = new Car('AAA', new Account('Andres', '123' , 'andres@email.com'))
 car.passager = 4
 car.printDataCar()
+
+let uberX = new UberX("BBB", new Account('Miguel', '123', 'Miguiel@gmail.com'), 'BM', 'i5' )
+uberX.passager = 4
+uberX.printDataCar()
